c25524f9bb4fb47e3ce5995377ca96144e72b702
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-15 15:45:42 -0400

    simple render

--- main.cc
+++ main.cc
@@ -1,6 +1,26 @@
 #include <iostream>
 
 int main(void) {
-    std::cout << "Hello world!\n";
+    // image
+    float image_width = 255.0;
+    float image_height = 255.0;
+
+    // render
+    std::cout << "P3\n" << image_width << ' ' << image_width << "\n255\n";
+
+    for (int height = 0; height < image_height; ++height) {
+        for (int width = 0; width < image_width; ++width) {
+            float r = double(width) / (image_width-1);
+            float g = double(height) / (image_height-1);
+            float b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
     return 0;
 }
