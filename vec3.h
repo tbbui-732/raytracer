d96e608dce8a7fbecc501b7efd8197bedb018fa5
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-16 12:48:41 -0400

    add operation assignments overloaded methods and length method

--- vec3.h
+++ vec3.h
@@ -17,19 +17,43 @@ class vec3 {
         double y() { return e[1]; }
         double z() { return e[2]; }
 
-        // overloading operators
-        vec3 operator-() const { 
+        vec3 operator-() const {                    // vector negation
             return vec3(-e[0], -e[1], -e[2]); 
         } 
-        double operator[](int i) const { 
+
+        double operator[](int i) const {            // copy of ith value
             return e[i];
         } 
-        double& operator[](int i) { 
+
+        double& operator[](int i) {                 // reference of ith value
             return e[i];
         } 
-        
 
+        vec3& operator+=(const vec3& newVec) {      // addition assignment
+            e[0] += newVec.e[0];
+            e[1] += newVec.e[1];
+            e[2] += newVec.e[2];
+            return *this;
+        }
 
+        vec3& operator*=(const int multiplier) {    // multiplication assignment
+            e[0] *= multiplier;
+            e[1] *= multiplier;
+            e[2] *= multiplier;
+            return *this;
+        }
+
+        vec3& operator/=(const int divisor) {       // division assignment
+            return (*this *= (1 / divisor));
+        }
+
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+        double length() const {
+            return std::sqrt(length_squared());
+        }
 };
 
 #endif
