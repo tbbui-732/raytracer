d4d5963ed5c3b30bd6f94850d73cb415d0823876
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-21 14:18:50 -0400

    created a red sphere

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,23 @@
 #include "../include/color.h"
 #include "../include/ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    // equation of sphere reduced to solve for t (discriminant)
+    // if ray missed sphere, it yields a negative value
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - (radius*radius);
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {                                                 // NOTE: creates an arbitrary blue to white gradient
+    // creates a red sphere
+    if (hit_sphere(point3(0,0,1), 0.5, r)) {
+        return color(1,0,0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + (a * color(0.5, 0.7, 1.0));
@@ -15,8 +31,8 @@ int main(void) {
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;                       // ensure image_height is >= 1
 
-    double viewport_height = 2.0;                                                 // camera/viewport dimensions
-    double viewport_width = viewport_height *                                     // NOTE; arbitrary value
+    double viewport_height = 2.0;                                               // camera/viewport dimensions
+    double viewport_width = viewport_height *                                   // NOTE; arbitrary value
                             ( double(image_width)/image_height );
     double focal_length = 1.0;
     point3 camera_center = point3(0, 0, 0);
@@ -41,7 +57,7 @@ int main(void) {
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
 
-    for (int height = 0; height < image_height; ++height) {
+    for (int height = 0; height < image_height; ++height) {                     // scan every pixel (left to right, top to bottom)
         std::clog << "\rScanlines remaining: "                                  // progress bar
                   << (image_height - height) 
                   << ' ' 
@@ -53,7 +69,7 @@ int main(void) {
                               + (height * pixel_delta_v);                       // move down by current height * size of vertical pixel delta
 
             auto ray_direction = pixel_center - camera_center;                  // calculate ray direction
-            ray r(camera_center, ray_direction);
+            ray r = ray(camera_center, ray_direction);
 
             color pixel_color = ray_color(r);                                   // calculate color of current pixel
             write_color(std::cout, pixel_color);
