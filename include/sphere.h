55f94c292ef7cfaee8b2173c68ef474bd21d1b02
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-21 16:01:35 -0400

    add comments to describe how sphere header works

--- include/sphere.h
+++ include/sphere.h
@@ -14,29 +14,37 @@ class sphere : public hittable {
         sphere(const point3& center, double radius) 
             : center(center), radius(std::fmax(0, radius)) {}
 
-        bool hit(const ray& r, double ray_tmin,                         // pure virtual function
+        bool hit(const ray& r, double ray_tmin,
                     double ray_tmax, hit_record& rec) const override {
-            vec3 oc = center - r.origin();
+
+            // Simplified calculation for a circle.
+            vec3 oc = center - r.origin(); // creates a vector from circle center to ray's origin.
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
             auto c = oc.length_squared() - radius*radius;
 
+            // Discriminant determines if the ray hit the sphere.
             auto discriminant = h*h - a*c;
             if (discriminant < 0) {
                 return false;
             }
 
-            auto sqrtd = std::sqrt(discriminant);
+            // Sqrt of discriminant (the roots) will be used to find-
+            //  the potential intersection points.
+            auto sqrt_discriminant = std::sqrt(discriminant);
+            auto root = (h - sqrt_discriminant) / a;
 
-            // find the nearest root that lies in the acceptable range
-            auto root = (h - sqrtd) / a;
+            // Check if the root(s) are within the valid range.
             if (root <= ray_tmin || ray_tmax <= root) {
-                root = (h + sqrtd) / a;
+                // Recalculate second root if first root is invalid.
+                root = (h + sqrt_discriminant) / a;
                 if (root <= ray_tmin || ray_tmax <= root) {
+                    // No hit if neither roots are valid.
                     return false;
                 }
             }
 
+            // Store intersection details.
             rec.t = root;
             rec.p = r.at(rec.t);
             rec.normal = (rec.p - center) / radius;
