d4084f37b677d5aa8b90c3c0bd05b17d732d0ad3
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-21 16:26:08 -0400

    feat: add front-face tracking to hit-record

--- include/hittable.h
+++ include/hittable.h
@@ -2,12 +2,22 @@
 #define HITTABLE_H
 
 #include "ray.h"
+#include "vec3.h"
 
 class hit_record {
     public:
         point3 p;
         vec3 normal;
         double t;
+        bool front_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            // Sets the hit record normal vector.
+            // NOTE: outward_normal is presumed to be unit length.
+            
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 
 class hittable {
--- include/sphere.h
+++ include/sphere.h
@@ -47,7 +47,8 @@ class sphere : public hittable {
             // Store intersection details.
             rec.t = root;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
 
             return true;
         }
