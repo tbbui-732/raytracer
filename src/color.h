f7f3b1bcd32548f6d8a9b42f259711c9dc910fca
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-24 16:24:20 -0400

    feat: lambertian diffuse and gamma space translation

--- src/color.h
+++ src/color.h
@@ -6,10 +6,16 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+    auto r = linear_to_gamma(pixel_color.x());
+    auto g = linear_to_gamma(pixel_color.y());
+    auto b = linear_to_gamma(pixel_color.z());
 
     // translate rgb values into byte range [0, 255]
     interval intensity(0.000, 0.999);
@@ -20,5 +26,6 @@ void write_color(std::ostream& out, const color& pixel_color) {
     // write out colors to ostream
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
+
 
 #endif
