f6ddb7693ef4bc334c07ba40bb54fc5d32c74b51
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-24 19:18:27 -0400

    spheres now initialize material type

--- src/sphere.h
+++ src/sphere.h
@@ -10,9 +10,8 @@ class sphere : public hittable {
         shared_ptr<material> mat;
 
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {
-            // TODO: initialize the material pointer 'mat'
-        }
+        sphere(const point3& center, double radius, shared_ptr<material> mat) 
+            : center(center), radius(std::fmax(0, radius)), mat(mat) {}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             // Simplified calculation for a circle.
