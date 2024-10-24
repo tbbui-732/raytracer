8517900560714427087b1e2c480d1d073ce904a2
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-24 15:18:15 -0400

    feat: antialiased sphere

--- src/camera.h
+++ src/camera.h
@@ -10,10 +10,14 @@ class camera {
         point3 pixel00_loc;    // Location of pixel 0, 0
         vec3   pixel_delta_u;  // Offset to pixel to the right
         vec3   pixel_delta_v;  // Offset to pixel below
+        double pixel_samples_scale;  // Color scale factor for a sum of pixel samples
+
 
         void initialize() {
             image_height = int(image_width / aspect_ratio);
             image_height = (image_height < 1) ? 1 : image_height;
+
+            pixel_samples_scale = 1.0 / samples_per_pixel;
 
             center = point3(0, 0, 0);
 
@@ -36,6 +40,26 @@ class camera {
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
 
+        vec3 sample_square() const {
+            // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+        }
+
+        ray get_ray(int i, int j) const {
+            // Construct a camera ray originating from the origin and directed at randomly sampled
+            // point around the pixel location i, j.
+
+            auto offset = sample_square();
+            auto pixel_sample = pixel00_loc
+                              + ((i + offset.x()) * pixel_delta_u)
+                              + ((j + offset.y()) * pixel_delta_v);
+
+            auto ray_origin = center;
+            auto ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
         color ray_color(const ray& r, const hittable& world) const {
             hit_record rec;
 
@@ -49,8 +73,9 @@ class camera {
         }
 
     public:
-        double aspect_ratio = 1.0;  // Ratio of image width over height
-        int    image_width  = 100;  // Rendered image width in pixel count
+        double aspect_ratio      = 1.0;  // Ratio of image width over height
+        int    image_width       = 100;  // Rendered image width in pixel count
+        int    samples_per_pixel = 10;   // Count of random samples for each pixel
 
         void render(const hittable& world) {
             initialize();
@@ -60,12 +85,12 @@ class camera {
             for (int j = 0; j < image_height; j++) {
                 std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
                 for (int i = 0; i < image_width; i++) {
-                    auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                    auto ray_direction = pixel_center - center;
-                    ray r(center, ray_direction);
-
-                    color pixel_color = ray_color(r, world);
-                    write_color(std::cout, pixel_color);
+                    color pixel_color(0,0,0);
+                    for (int sample = 0; sample < samples_per_pixel; sample++) {
+                        ray r = get_ray(i, j);
+                        pixel_color += ray_color(r, world);
+                    }
+                    write_color(std::cout, pixel_samples_scale * pixel_color);
                 }
             }
 
--- src/rtweekend.h
+++ src/rtweekend.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -16,6 +17,16 @@ const double pi = 3.1415926535897932385;
 // Utility functions.
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // random real between [0, 1)
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // random real between [min, max)
+    return min + (max-min) * random_double();
 }
 
 // Common headers.
