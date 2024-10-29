dbea36bbe4f884125037466e871ff306f1162daf
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-10-29 18:59:33 -0400

    define a method for getting a random vector for x and y plane

--- src/vec3.h
+++ src/vec3.h
@@ -132,7 +132,17 @@ inline vec3 cross(const vec3& u, const vec3& v) {
                 u.e[0]*v.e[1] - u.e[1]*v.e[0]);
 }
 
-inline vec3 unit_vector(const vec3& v) { return v / v.length(); }
+inline vec3 unit_vector(const vec3& v) { 
+    return v / v.length(); 
+}
+
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
 
 inline vec3 random_unit_vector() {
     while (true) {
